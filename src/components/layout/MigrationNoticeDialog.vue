28cd100f546f223b6c2b0d0f174d858155ae2554
NCreeper233 <nuclear.creeper_233@outlook.com>
2026-08-27 20:52:46 +0800

    感谢赛博神仙保佑

--- src/components/layout/MigrationNoticeDialog.vue
+++ src/components/layout/MigrationNoticeDialog.vue
@@ -11,6 +11,16 @@ import { ref } from 'vue'
 
 /** "不再显示"标记的 localStorage key */
 const DISMISSED_KEY = 'endfield-baker-settings-migration-notice-dismissed'
+/** 公告版本号:每次更新公告内容时递增,强制已点击"不再显示"的用户重新看到 */
+const NOTICE_VERSION = 2
+const VERSION_KEY = 'endfield-baker-settings-migration-notice-version'
+
+/** 版本不匹配时清除旧标记,确保新公告必定弹出 */
+const savedVersion = Number(localStorage.getItem(VERSION_KEY) || '0')
+if (savedVersion < NOTICE_VERSION) {
+  localStorage.removeItem(DISMISSED_KEY)
+  localStorage.setItem(VERSION_KEY, String(NOTICE_VERSION))
+}
 
 /** 是否展示弹窗:进入时读取 localStorage,"不再显示"过则不弹 */
 const open = ref(!localStorage.getItem(DISMISSED_KEY))
@@ -37,7 +47,7 @@ function onDismissForever() {
       <div class="mn__panel">
         <h2 class="mn__title">公告</h2>
         <p class="mn__text">
-          请在使用本网站的用户务必加入QQ群：<strong class="mn__qq">926147159</strong> 以获取相关的通知公告！<br>重要的事情说三遍：请一定加入QQ群！<br>群号：926147159<br>群号：926147159<br>群号：926147159
+          请在使用本网站的用户务必加入QQ群：<strong class="mn__qq">926147159</strong> 以获取相关的通知公告！<br>重要的事情说三遍：请一定加入QQ群！<br>群号：926147159<br>群号：926147159<br>群号：926147159<br><br>最好多导出保存一下数据，因为不知道赛博神仙能保佑多久，以防数据出什么问题<br>感谢赛博神仙保佑！<br>按E键可以隐藏（手机端不用管）
         </p>
         <div class="mn__actions">
           <button class="mn__btn mn__btn--primary" type="button" @click="onConfirm">确定</button>
